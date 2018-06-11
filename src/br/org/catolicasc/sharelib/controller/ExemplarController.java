b377fb277f305949a1362ab8dd3926b0b68eeec5
rriese <rafael.riese@catolicasc.org.br>
2018-06-10 22:16:33 -0300

    Added crud exemplar

--- src/br/org/catolicasc/sharelib/controller/ExemplarController.java
+++ src/br/org/catolicasc/sharelib/controller/ExemplarController.java
@@ -7,6 +7,7 @@ import javax.faces.bean.ManagedBean;
 import br.org.catolicasc.sharelib.jpa.QueryDataModel;
 import br.org.catolicasc.sharelib.bean.Exemplar;
 import br.org.catolicasc.sharelib.bean.Livro;
+import br.org.catolicasc.sharelib.bean.Status;
 import br.org.catolicasc.sharelib.dao.ExemplarDao;
 import br.org.catolicasc.sharelib.dao.LivroDao;
 
@@ -53,5 +54,9 @@ public class ExemplarController {
 	
 	public List<Livro> getLivros() {
 		return new LivroDao().listaTodos();
+	}
+	
+	public Status[] getStatus() {
+		return Status.values();
 	}
 }
